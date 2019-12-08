9efb350564feb59a676124f28b32cef67d0dfc0f
Johan Van den Neste <jvdneste@gmail.com>
2019-12-08 16:39:12 +0100

    day 2

--- advent-of-code-2019-clojure/src/advent_of_code_2019/day_02.clj
+++ advent-of-code-2019-clojure/src/advent_of_code_2019/day_02.clj
@@ -3,17 +3,19 @@
 
 (def program-text "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,6,23,2,23,6,27,2,6,27,31,2,13,31,35,1,9,35,39,2,10,39,43,1,6,43,47,1,13,47,51,2,6,51,55,2,55,6,59,1,59,5,63,2,9,63,67,1,5,67,71,2,10,71,75,1,6,75,79,1,79,5,83,2,83,10,87,1,9,87,91,1,5,91,95,1,95,6,99,2,10,99,103,1,5,103,107,1,107,6,111,1,5,111,115,2,115,6,119,1,119,6,123,1,123,10,127,1,127,13,131,1,131,2,135,1,135,5,0,99,2,14,0,0")
 
-(def program-values (str/split program-text #","))
+(def program-values
+  (-> (str/split program-text #",") (assoc 1 12) (assoc 2 2)))
 
 (defn program-terminates [program position]
   (= (nth program position) 99))
 
 (defn program-eval-at-position [program position]
-  (let [opcode (nth program position) arg1 (nth program (+ position 1)) arg2 (nth program (+ position 2)) result-position (nth program (+ position 3))]
-    (case opcode
-      1 (assoc program result-position (+ arg1 arg2))
-      2 (assoc program result-position (* arg1 arg2))
-      program)))
+  (let [opcode (nth program position) ptr1 (nth program (+ position 1)) ptr2 (nth program (+ position 2)) result-position (nth program (+ position 3))]
+    (let [arg1 (nth program ptr1) arg2 (nth program ptr2)]
+      (case opcode
+        1 (assoc program result-position (+ arg1 arg2))
+        2 (assoc program result-position (* arg1 arg2))
+        program))))
 
 (defn program-eval-from-position-to-end [program position]
   (if (>= position (count program))
@@ -22,6 +24,6 @@
       program
       (program-eval-from-position-to-end (program-eval-at-position program position) (+ position 4)))))
 
-(def result (program-eval-from-position-to-end program-values 0))
+(defn program-eval "evaluate a complete program" [program] (program-eval-from-position-to-end program 0))
 
-(println result)
+;(println "result" (program-eval program-values))
---
+++ advent-of-code-2019-clojure/test/advent_of_code_2019/day_02_test.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code-2019.day-02-test
+  (:require [clojure.test :refer :all])
+  (:require [advent-of-code-2019.day-02 :refer :all]))
+
+(deftest program-eval-at-position-tests
+  (testing "program-eval-at-position basic tests"
+    (is (= [2 0 0 0 99] (program-eval [1 0 0 0 99])))
+    (is (= [2 3 0 6 99] (program-eval [2 3 0 3 99])))
+    (is (= [2 4 4 5 99 9801] (program-eval [2 4 4 5 99 0])))
+    (is (= [30 1 1 4 2 5 6 0 99] (program-eval [1 1 1 4 99 5 6 0 99])))))
+
+;(deftest total-fuel-is-correct
+;  (testing "eval some basic programs"
+;    (is (= [2 0 0 0 99] (program-eval [1 0 0 0 99])))))
